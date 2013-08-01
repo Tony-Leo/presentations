af4f660d063c39c433d39bdbc784272ba4e31e73
salami162 <limin.shen@hotmail.com>
2013-08-01 16:34:57 -0700

    create firebird page

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 // Configuration
-app.configure(function(){
+app.configure(function () {
   app.set('title', 'Limin Shen');
   app.set('views', __dirname + '/public');
   app.set('view engine', 'jade');
   app.use(express.methodOverride());
   app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express['static'](__dirname + '/public'));
 });
 
 // setup server routes
@@ -19,17 +19,15 @@ app.configure(function(){
 
 // session support
 app.use(express.cookieParser());
-app.use(express.session({ secret: "mathday" }));
+app.use(express.session({ secret: "limin.shen" }));
 
 
-app.configure('development', function(){
-    app.use(express.static(__dirname + '/public')); //dir of the currently executing script.
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+  app.use(express['static'](__dirname + '/public')); //dir of the currently executing script.
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 
 app.listen(1234);
 
 console.log('Server running @ http://127.0.0.1:1234');
-
-
